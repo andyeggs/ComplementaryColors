0ffd8d08d512c689caab5620a7e1eec31e0d9b4d
Andy E <andyjohneggs@gmail.com>
2019-05-14 08:30:11 -0500

    Refactored and deleted the "A", ANDY

--- app/src/main/java/com/ctech/complementarycolors/MainActivity.java
+++ app/src/main/java/com/ctech/complementarycolors/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
     //Reference the seek bars
-    SeekBar SeekA;
     SeekBar SeekR;
     SeekBar SeekG;
     SeekBar SeekB;
@@ -20,45 +19,40 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.color_picker_main);
         //Get a reference to the seekbars
-        SeekA=(SeekBar)findViewById(R.id.seekA);
         SeekR=(SeekBar)findViewById(R.id.seekR);
         SeekG=(SeekBar)findViewById(R.id.seekG);
         SeekB=(SeekBar)findViewById(R.id.seekB);
         //Reference the TextView
         ShowColor=(TextView)findViewById(R.id.textView);
         //This activity implements SeekBar OnSeekBarChangeListener
-        SeekA.setOnSeekBarChangeListener(this);
         SeekR.setOnSeekBarChangeListener(this);
         SeekG.setOnSeekBarChangeListener(this);
         SeekB.setOnSeekBarChangeListener(this);
     }
     //Satisfy the implements
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
-        //get current ARGB values
-        int A=SeekA.getProgress();
+        //get current RGB values
         int R=SeekR.getProgress();
         int G=SeekG.getProgress();
         int B=SeekB.getProgress();
         //Reference the value changing
         int id=seekBar.getId();
         //Get the chnaged value
-        if(id == com.ctech.complementarycolors.R.id.seekA)
-            A=progress;
-        else if(id == com.ctech.complementarycolors.R.id.seekR)
+        if(id == com.ctech.complementarycolors.R.id.seekR)
             R=progress;
-        else if(id == com.ctech.complementarycolors.R.id.seekA)
+        else if(id == com.ctech.complementarycolors.R.id.seekG)
             G=progress;
-        else if(id == com.ctech.complementarycolors.R.id.seekA)
+        else if(id == com.ctech.complementarycolors.R.id.seekB)
             B=progress;
         //Build and show the new color
-        ShowColor.setBackgroundColor(Color.argb(A,R,G,B));
+        ShowColor.setBackgroundColor(Color.rgb(R,G,B));
         //show the color value
-        ShowColor.setText("0x"+String.format("%02x", A)+String.format("%02x", R)
+        ShowColor.setText(String.format("%02x", R)
                 +String.format("%02x", G)+String.format("%02x", B));
         //some math so text shows (needs improvement for greys)
-        ShowColor.setTextColor(Color.argb(0xff,255-R,255-G,255-B));
+        ShowColor.setTextColor(Color.rgb(255-R,255-G,255-B));
     }
     public void onStartTrackingTouch(SeekBar seekBar) {
         //Only required due to implements
@@ -81,7 +75,6 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
             //ShowColor.setBackgroundColor(color);
             //show the color value
             ShowColor.setText("0x"+String.format("%08x", color));
-            SeekA.setProgress(Color.alpha(color));
             SeekR.setProgress(Color.red(color));
             SeekG.setProgress(Color.green(color));
             SeekB.setProgress(Color.blue(color));
